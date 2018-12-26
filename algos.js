2b2106d83f67bb62cf19eb7e0f4c8c1534472070
Mariam Joan <mariamjoan@Mariams-MBP-2.fios-router.home>
2018-12-26 00:58:45 -0500

    quicksort pseudo

--- algos.js
+++ algos.js
@@ -40,7 +40,7 @@ const partition = (a, pivot, left, right) => {
 
     while(left <= right) {
         if(a[left] < pivot) {
-            left--
+            left++
         }
         if(a[right] > pivot) {
             right--
@@ -76,3 +76,32 @@ const swap = (a, i, j) => {
 array = [98, 45, 34, 23, 12, 33, 87, 99, 65, 37]  
 console.log(quick(array));
 // [ 12, 23, 33, 34, 37, 45, 65, 87, 98, 99 ]
+
+/*
+PART I: 
+  declare partition(array, i, j, pivot)
+  pick pivot (methods vary: random, mid, first, etc.)
+  pick pointers (left(i) and right(j) in array)
+
+      if(i < pivot) then i++  			    i -> left LESS than pivot then i increment
+      if(i > pivot) then stop         		stop check j first 
+          if(j > pivot) then j--  	 		j -> right GREATER than pivot then j decrement
+          if(j < pivot) then stop   		stop, look at i	
+      swap(i, j)  				            swap
+      continue; 
+
+PART II:
+  do pointers meet? 
+  if(i >= j) 			                    after the final swaps if i is LESS than j then i crossed paths with j!  
+  swap (i, j)
+      else 
+          swap(pivot, i)		            now there is nothing greater than j so we swap pivot
+          return i 		                    index of item in place!
+
+PART III:  
+  declare quicksort(array, i, j)
+  if (j <= i) return;
+  int j = partition(array, i, j);
+  sort(array, i, j-1)
+  sort(array, j+1, j)
+*/ 
